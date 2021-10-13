9f53f581eac4a5803bcf6ba1ec58531370bb8326
Guangxin Liu <gl19@illinois.edu>
2021-10-13 11:25:39 -0500

    asdf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Searchbar from './components/Searchbar/Searchbar';
